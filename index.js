a7ad08847e03e780a18200bd5b8e69efec742f75
jzolo22 <jzolotarev@gmail.com>
2020-11-15 13:22:32 -0500

    commit tree e49bafc3148e6ea32fe163438b312184522a6e10

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+const num3 = 20
+const num4 = 8
+
+let mod = num3 % num4
+
+let max = Math.max(10, 12, 20)
